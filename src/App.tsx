3c2d032e70e4417f01a89fd88f2e9fdb7f38eb7f
chloe41297 <79011228+chloe41297@users.noreply.github.com>
2022-03-22 23:04:33 +0900

    feat: pixel 진입 home page 제작

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./App.css";
 import PixelMain from "./pages/PixelMain";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PixelDetails from "pages/PixelDetails";
+import Home from "pages/Home";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home></Home>}></Route>
           <Route path="/pixel" element={<PixelMain></PixelMain>}></Route>
           <Route
             path="/pixel/products/:keyword"
